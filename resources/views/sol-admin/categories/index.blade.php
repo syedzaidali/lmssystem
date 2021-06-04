e2be0094eb2813f2686ef4c393c495b60c9cd5cd
Raoburhan58 <burhanrao58@gmail.com>
2021-06-04 15:07:19 -0700

    add catagoery in admin dashboard

--- resources/views/sol-admin/categories/index.blade.php
+++ resources/views/sol-admin/categories/index.blade.php
@@ -1,35 +1,54 @@
-<!DOCTYPE html>
-<html>
-<head>
+@extends('layouts.soladmin')
 
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
-    <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
-    <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
-    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
-</head>
-<body>
 
-<div class="container">
+@section('sol-style')
+<meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <a class="btn btn-success" href="javascript:void(0)" id="createNewProduct"> Create New Categoeies</a>
-    <table class="table table-bordered data-table">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Name</th>
-                <th>Heading</th>
-                <th>Description</th>
-                <th width="280px">Action</th>
-            </tr>
-        </thead>
-        <tbody>
-        </tbody>
-    </table>
+
+@endsection
+
+@section('sol-content')
+
+
+<div class="our-dashbord dashbord">
+    <div class="dashboard_main_content">
+        <div class="container-fluid">
+            <div class="main_content_container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <nav class="breadcrumb_widgets" aria-label="breadcrumb mb30">
+                            <h4 class="title float-left">Catagoery</h4>
+                            <ol class="breadcrumb float-right">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+                            </ol>
+                        </nav>
+                    </div>
+                    <div class="col-lg-12 my-3" style="text-align:end;">
+                        <a class="btn btn-transparent divider-btn" href="javascript:void(0)" id="createNewProduct"
+                            style="border: 2px solid #2441e7;">
+                            Create New Categoeies
+                        </a>
+                    </div>
+                </div>
+
+
+                <table class="table table-bordered data-table">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Name</th>
+                            <th>Heading</th>
+                            <th>Description</th>
+                            <th width="280px">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
 
 <div class="modal fade" id="ajaxModel" aria-hidden="true">
@@ -40,42 +59,45 @@
             </div>
             <div class="modal-body">
                 <form id="productForm" name="productForm" class="form-horizontal">
-                   <input type="hidden" name="product_id" id="product_id">
+                    <input type="hidden" name="product_id" id="product_id">
                     <div class="form-group">
                         <label for="name" class="col-sm-2 control-label">Name</label>
                         <div class="col-sm-12">
-                            <input type="text" class="form-control" id="name" name="name" placeholder="Enter Name" value="" maxlength="50" required="">
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Enter Name"
+                                value="" maxlength="50" required="">
                         </div>
                     </div>
 
                     <div class="form-group">
                         <label class="col-sm-2 control-label">Heading</label>
                         <div class="col-sm-12">
-                            <input type="text" id="heading" name="heading" required="" placeholder="Enter Heading" class="form-control">
+                            <input type="text" id="heading" name="heading" required="" placeholder="Enter Heading"
+                                class="form-control">
                         </div>
                     </div>
                     <div class="form-group">
                         <label class="col-sm-2 control-label">Description</label>
                         <div class="col-sm-12">
-                            <input type="text" id="description" name="description" required="" placeholder="Enter Description" class="form-control">
-                            <input type="hidden" id="photo" name="photo" value="1" required="" placeholder="Enter Description" class="form-control">
+                            <input type="text" id="description" name="description" required=""
+                                placeholder="Enter Description" class="form-control">
+                            <input type="hidden" id="photo" name="photo" value="1" required=""
+                                placeholder="Enter Description" class="form-control">
                         </div>
                     </div>
                     <div class="form-group">
                         <label class="col-sm-2 control-label">Status</label>
                         <div class="col-sm-12">
-                            <select name="status" id="status">
+                            <select name="status" id="status" >
                                 <option value="">Select Status</option>
                                 <option value="1">Actived</option>
                                 <option value="0">Deactived</option>
                             </select>
-
                         </div>
                     </div>
 
                     <div class="col-sm-offset-2 col-sm-10">
-                     <button type="submit" class="btn btn-primary" id="saveBtn" value="create">Save changes
-                     </button>
+                        <button type="submit" class="btn btn-primary" id="saveBtn" value="create">Save changes
+                        </button>
                     </div>
                 </form>
             </div>
@@ -83,31 +105,60 @@
     </div>
 </div>
 
-</body>
 
+
+
+
+@endsection
+
+@section('sol-script')
+
+<link rel="stylesheet"href="{{asset('https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css')}}" />
+<link href="{{asset('https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css')}}" rel="stylesheet">
+<link href="{{asset('https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
+<script src="{{asset('https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js')}}"></script>
+
+<script src="{{asset('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js')}}"></script>
 <script type="text/javascript">
-  $(function () {
+$(function() {
 
-      $.ajaxSetup({
-          headers: {
-              'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-          }
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
     });
 
     var table = $('.data-table').DataTable({
         processing: true,
         serverSide: true,
         ajax: "{{ route('ajaxproducts.index') }}",
-        columns: [
-            {data: 'DT_RowIndex', name: 'DT_RowIndex'},
-            {data: 'name', name: 'name'},
-            {data: 'heading', name: 'heading'},
-            {data: 'description', name: 'description'},
-            {data: 'action', name: 'action', orderable: false, searchable: false},
+        columns: [{
+                data: 'DT_RowIndex',
+                name: 'DT_RowIndex'
+            },
+            {
+                data: 'name',
+                name: 'name'
+            },
+            {
+                data: 'heading',
+                name: 'heading'
+            },
+            {
+                data: 'description',
+                name: 'description'
+            },
+            {
+                data: 'action',
+                name: 'action',
+                orderable: false,
+                searchable: false
+            },
         ]
     });
 
-    $('#createNewProduct').click(function () {
+    $('#createNewProduct').click(function() {
         $('#saveBtn').val("create-product");
         $('#product_id').val('');
         $('#productForm').trigger("reset");
@@ -115,61 +166,63 @@
         $('#ajaxModel').modal('show');
     });
 
-    $('body').on('click', '.editProduct', function () {
-      var product_id = $(this).data('id');
-      $.get("{{ route('ajaxproducts.index') }}" +'/' + product_id +'/edit', function (data) {
-          $('#modelHeading').html("Edit Product");
-          $('#saveBtn').val("edit-user");
-          $('#ajaxModel').modal('show');
-          $('#product_id').val(data.id);
-          $('#name').val(data.name);
-          $('#heading').val(data.heading);
-          $('#description').val(data.description);
-          $('#photo').val(data.photo);
-          $('#status').val(data.status);
-      })
-   });
+    $('body').on('click', '.editProduct', function() {
+        var product_id = $(this).data('id');
+        $.get("{{ route('ajaxproducts.index') }}" + '/' + product_id + '/edit', function(data) {
+            $('#modelHeading').html("Edit Product");
+            $('#saveBtn').val("edit-user");
+            $('#ajaxModel').modal('show');
+            $('#product_id').val(data.id);
+            $('#name').val(data.name);
+            $('#heading').val(data.heading);
+            $('#description').val(data.description);
+            $('#photo').val(data.photo);
+            $('#status').val(data.status);
+        })
+    });
 
-    $('#saveBtn').click(function (e) {
+    $('#saveBtn').click(function(e) {
         e.preventDefault();
         $(this).html('Sending..');
 
         $.ajax({
-          data: $('#productForm').serialize(),
-          url: "{{ route('ajaxproducts.store') }}",
-          type: "POST",
-          dataType: 'json',
-          success: function (data) {
+            data: $('#productForm').serialize(),
+            url: "{{ route('ajaxproducts.store') }}",
+            type: "POST",
+            dataType: 'json',
+            success: function(data) {
 
-              $('#productForm').trigger("reset");
-              $('#ajaxModel').modal('hide');
-              table.draw();
+                $('#productForm').trigger("reset");
+                $('#ajaxModel').modal('hide');
+                table.draw();
 
-          },
-          error: function (data) {
-              console.log('Error:', data);
-              $('#saveBtn').html('Save Changes');
-          }
-      });
+            },
+            error: function(data) {
+                console.log('Error:', data);
+                $('#saveBtn').html('Save Changes');
+            }
+        });
     });
 
-    $('body').on('click', '.deleteProduct', function () {
+    $('body').on('click', '.deleteProduct', function() {
 
         var product_id = $(this).data("id");
         confirm("Are You sure want to delete !");
 
         $.ajax({
             type: "DELETE",
-            url: "{{ route('ajaxproducts.store') }}"+'/'+product_id,
-            success: function (data) {
+            url: "{{ route('ajaxproducts.store') }}" + '/' + product_id,
+            success: function(data) {
                 table.draw();
             },
-            error: function (data) {
+            error: function(data) {
                 console.log('Error:', data);
             }
         });
     });
 
-  });
+});
 </script>
-</html>
+
+
+@endsection
